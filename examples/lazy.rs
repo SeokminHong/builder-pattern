8b180211258bdda37c035d9739cb11d7c63b7a82
Seokmin Hong <ghdtjrals240@naver.com>
2021-09-27 21:20:52 +0900

    Improve readability

--- examples/lazy.rs
+++ examples/lazy.rs
@@ -14,6 +14,8 @@ struct Person {
     // #[setter(value, lazy, async)]
     name: String,
     age: u8,
+    // Default value is lazy evaluated.
+    // Only lazy setter is provided.
     // #[default_lazy(|| "Hello")]
     // #[setter(lazy)]
     address: &'static str,
@@ -86,7 +88,7 @@ impl<'a, AsyncField, T1, T3> PersonBuilder<'a, AsyncField, T1, (), T3> {
 }
 
 impl<'a, AsyncField, T1, T2> PersonBuilder<'a, AsyncField, T1, T2, ()> {
-    pub fn address(
+    pub fn address_lazy(
         self,
         value: fn() -> &'static str,
     ) -> PersonBuilder<'a, AsyncField, T1, T2, &'static str> {
@@ -141,23 +143,24 @@ impl<'a, T3> PersonBuilder<'a, AsyncBuilder, String, u8, T3> {
 
 #[tokio::main]
 async fn main() {
-    // Synchronous builder
-    let a_builder = Person::new()
-        .name(String::from("Jack"))
-        .age(30)
-        .address(|| "New York");
+    // `name` is evaluated here
+    let a_builder = Person::new().name(String::from("Jack")).age(30);
+    let a = a_builder.build(); // `address` is evaluated here
+    println!("{:?}", a);
+
     // Lazy builder
-    let b_builder = Person::new().name_lazy(|| String::from("Jane")).age(50);
+    let b_builder = Person::new()
+        .name_lazy(|| String::from("Jane"))
+        .age(50)
+        .address_lazy(|| "New York");
+    let b = b_builder.build(); // `name` and `address` is evaluated here
+    println!("{:?}", b);
+
     // Asynchronous builder
     let c_builder = Person::new()
         .name_async(|| async { String::from("Joe") })
         .age(17)
-        .address(|| "Tokyo");
-
-    let a = a_builder.build();
-    println!("{:?}", a);
-    let b = b_builder.build();
-    println!("{:?}", b);
-    let c = c_builder.build().await;
+        .address_lazy(|| "Tokyo");
+    let c = c_builder.build().await; // `name` and `address` is evaluated here
     println!("{:?}", c);
 }
