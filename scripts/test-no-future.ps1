61e356d8a9c93edb9c13204a90a96a05e446744c
Seokmin Hong <ghdtjrals240@naver.com>
2021-10-02 17:44:36 +0900

    Update windows CI

---
+++ scripts/test-no-future.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = 'SilentlyContinue'
+Push-Location $PSScriptRoot\..\test-no-future\
+Get-ChildItem examples -Filter *.rs |
+Foreach-Object {
+  cargo run --example $_.BaseName
+  if ($_.BaseName.StartsWith('fail-')) {
+    if ($LASTEXITCODE -eq 0) {
+      Write-Output "Error: example '$_' should fail!"
+      $Host.SetShouldExit(1)
+      Pop-Location
+      exit
+    } else {
+      Write-Output 'Failed!'
+      $Host.SetShouldExit(0)
+    }
+  } else {
+    if ($LASTEXITCODE -ne 0) {
+      $Host.SetShouldExit($LASTEXITCODE)
+      Pop-Location
+      exit
+    }
+  }
+}
+Pop-Location
